af298e7e1d6c25d1c71112d753341a6f271769cd
paramsxngha <param.ssangha@outlook.com>
2025-03-14 08:59:03 +1300

    V1.08

--- WagesApp/Program.cs
+++ WagesApp/Program.cs
@@ -4,73 +4,114 @@ namespace WagesApp;
 
 class Program
 {
-    //Global Variables
-    static List<string> DAYS = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
-    //Constant Variable
+    // Global variables
+    static List<string> DAYS = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
+    static float sumWages = 0;
+
+    // Constant Variable
     static readonly float PAYRATE = 22.00f, TAXA = 0.075f, TAXB = 0.08f;
 
-    //Methods or Functions
+    // Methods or Functions
+
+    static string CheckProceed()
+    {
+        string proceed;
+        while (true)
+        {
+            Console.WriteLine("Press <Enter> to add another employee's informaton or type 'Stop' to quit.");
+            proceed = Console.ReadLine().ToUpper();
+
+            if (proceed.Equals("") || proceed.Equals("STOP")
+            {
+                return proceed;
+            }
+
+
+
+            Console.WriteLine("Error: Invalid input.");
+        }
+    }
+
 
     static int CheckHoursWorked(string day)
     {
         while (true)
         {
-            Console.WriteLine($"Enter the hours worked on {day}:");
-            int hoursWorked = Convert.ToInt32(Console.ReadLine());
-
-            if(hoursWorked >= 0 && hoursWorked  <= 24)
+            try
             {
-                return hoursWorked;
-            }
+                Console.WriteLine($"\nEnter the hours worked on {day}:");
+                int hoursWorked = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Error: Hours worked must de between 0 and 24");
+                if (hoursWorked >= 0 && hoursWorked <= 24)
+                {
+                    return hoursWorked;
+                }
+
+                Console.WriteLine("Error: Hours worked must be between 0 and 24");
+            }
+            catch
+            {
+                Console.WriteLine("Error: You must enter a valid number");
+            }
 
         }
     }
-
-    static string CheckName(string name)
+    static string CheckName()
     {
+        //local declarations
+        string employeeName;
         TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
 
-        return textInfo.ToTitleCase(name);
-    }
+        while (true)
+        {
+            Console.WriteLine("Enter the employee's name:");
 
-  
+            employeeName = textInfo.ToTitleCase(Console.ReadLine());
+
+            if (!employeeName.Equals("") && !employeeName.Any(char.IsDigit))
+            {
+                return employeeName;
+            }
+
+            Console.WriteLine("Error: Invalid name entered");
+
+        }
+
+
+
+    }
     static string FormatToDollar(float value)
     {
         return string.Format("{0:0.00}", value);
     }
-
-
     static string PaySummary(string name, List<int> hrsWorked)
     {
-      return "----- Pay Summary -----\n" +
+        sumWages += CalculateWages(hrsWorked) + CalculateBonus(hrsWorked);
+
+        return "\n----- Pay Summary ----\n" +
             $"Employee Name: {name}\n" +
             $"Hours Worked: {SumHoursWorked(hrsWorked)}\n" +
             $"Bonus Owed: ${FormatToDollar(CalculateBonus(hrsWorked))}\n" +
             $"Gross Pay: ${FormatToDollar(CalculateWages(hrsWorked) + CalculateBonus(hrsWorked))}\n" +
-            $"Net Pay: ${FormatToDollar(CalculateWages(hrsWorked) + CalculateBonus(hrsWorked) - CalculateTax(hrsWorked))}\n" +
-            $"Tax Owed: ${FormatToDollar(CalculateTax(hrsWorked))}";
+            $"Tax Owed: ${FormatToDollar(CalculateTax(hrsWorked))}\n" +
+            $"Net Pay: ${FormatToDollar(CalculateWages(hrsWorked) + CalculateBonus(hrsWorked) - CalculateTax(hrsWorked))}";
+
     }
-    //Calculate tax (pay <450 then 7.5% tax else tax =8%
+
+    // Calculate tax (pay < 450 then tax = 7.5% else tax = 8%)
     static float CalculateTax(List<int> hrsWorked)
-            //Calculate weekly wages (total hours x pay rate)
     {
 
 
-
-        if(CalculateWages(hrsWorked)+CalculateBonus(hrsWorked)< 450)
+        if (CalculateWages(hrsWorked) + CalculateBonus(hrsWorked) < 450)
         {
-            return (float)Math.Round((CalculateWages(hrsWorked) + CalculateBonus(hrsWorked)) * TAXA);
+            return (float)Math.Round((CalculateWages(hrsWorked) + CalculateBonus(hrsWorked)) * TAXA, 2);
         }
 
-        return (float)Math.Round((CalculateWages(hrsWorked) + CalculateBonus(hrsWorked)) * TAXB);
-
-
+        return (float)Math.Round((CalculateWages(hrsWorked) + CalculateBonus(hrsWorked)) * TAXB, 2);
     }
 
     // Determine if employee qualifies for a bonus (>30 hours for the week)
-
     static float CalculateBonus(List<int> hrsWorked)
     {
 
@@ -80,62 +121,73 @@ class Program
         }
 
         return 0;
-
     }
 
-    //Calculate weekly wages (total hours x pay rate)
+    // Calculate weekly wages (total hours x pay rate)
     static float CalculateWages(List<int> hrsWorked)
     {
         return (float)Math.Round(SumHoursWorked(hrsWorked) * PAYRATE, 2);
-     //Calculate total hours worked
     }
+
+    // Calculate total hours worked
     static int SumHoursWorked(List<int> hrsWorked)
     {
-        int SumHoursWorked = 0;
+        int sumHoursWorked = 0;
 
         foreach (int hour in hrsWorked)
         {
-            SumHoursWorked += hour;
+            sumHoursWorked += hour;
         }
 
-        return SumHoursWorked;
+        return sumHoursWorked;
     }
-
     static void OneEmployee()
     {
-        //local variables
+        // Local variables
         string employeeName;
         List<int> hoursWorked = new List<int>();
 
-        //Input employee name
-        Console.WriteLine("Enter The Employee's Name:/n");
-        employeeName = CheckName(Console.ReadLine());
+        // Input employee name
 
-        //Input the number of hours worked for each day
-        foreach (string day in DAYS)
+        employeeName = CheckName();
+
+        // Input the number of hours worked for each day
+        foreach (var day in DAYS)
         {
+
             hoursWorked.Add(CheckHoursWorked(day));
+
         }
-        
 
-
-        //Display employees pay summary
+        // Display employees pay summary
         Console.WriteLine(PaySummary(employeeName, hoursWorked));
+    }
+
+    // When Run...
+    static void Main(string[] args)
+    {
+        DAYS.AsReadOnly();
+
+        string proceed = "";
+        while (proceed.Equals(""))
+        {
+            // Call OneEmployee Method
+            OneEmployee();
+
+            Console.WriteLine("Press <Enter> to add another employee's information or type 'Stop' to quit.");
+            proceed = Console.ReadLine();
+        }
+
+
+
+        // Display total amount paid to all employees
+        Console.WriteLine($"Total amount paid to employees: ${FormatToDollar(sumWages)}");
+
+        // Display highest paid employee
 
         Console.ReadLine();
     }
-
-    //When Run...
-
-    static void Main(string[] args)
-    {
-        OneEmployee();
-
-        //Display total amount paiyed to all employees
-
-        //Display highest paiyed employee
-
-
-    }
 }
+
+
 
